3fc60e1dfd0b6cdb3841531524040b93cd189224
Kimon Haars <kimon.haars@gmail.com>
2021-01-12 21:32:32 +0100

    two routes, controller methods and views created, but new view doesn't have form yet

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,19 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(params)
+    @restaurant.save
+  end
+
+
+  private
+
+  # def params_
+end
